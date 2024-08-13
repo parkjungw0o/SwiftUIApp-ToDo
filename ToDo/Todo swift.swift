1884d5a5287dd75467781181835075a940f921e5
hotcat0421 <s24044@gsm.hs.kr>
2024-08-13 20:40:09 +0900

    init

---
+++ ToDo/Todo swift.swift
@@ -0,0 +1,8 @@
+//
+//  Todo swift.swift
+//  ToDo
+//
+//  Created by 박정우 on 8/5/24.
+//
+
+import Foundation
---
+++ ToDo/TodoDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoDetailView.swift
+//  ToDo
+//
+//  Created by 박정우 on 8/13/24.
+//
+
+import SwiftUI
+
+struct TodoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoDetailView()
+}
